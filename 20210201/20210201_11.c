761a842a9cea45608e0beaa88b18229379fd087f
todorova98 <76940219+todorova98@users.noreply.github.com>
2021-02-04 10:23:49 +0200

    Update 20210201_11.c

--- 20210201/20210201_11.c
+++ 20210201/20210201_11.c
@@ -5,13 +5,17 @@
 void squeeze(char s[], int c);
 int main(){
     char str[]="Hello!";
-    squeeze(str,3);
+    squeeze(str,'l');
     return 0;
 }
 void squeeze(char s[], int c){
-    for(int i=c; i<strlen(s);i++){
-        s[i]=s[i+1];
-
+    int j=0;
+    for(int i=0; i<strlen(s);i++){
+            if(s[i]!=c){
+                s[j]=s[i];
+                j++;
+            }
         }
+        s[j]='\0';
         printf("%s",s);
     }
