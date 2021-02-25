1ff01563473f4f2c0fa14c06824c322dca1524b3
todorova98 <76940219+todorova98@users.noreply.github.com>
2021-02-25 10:33:42 +0200

    Create 20210224_1.c

---
+++ 20210224/20210224_1.c
@@ -0,0 +1,11 @@
+/*1. Дефинирайте константи PI, Е, и други с помощта на макроси. Използвайте
+ги в кода.*/
+#include <stdio.h>
+#define PI (3.141592)
+#define E (2.718281)
+int main(){
+    double pi=PI;
+    printf("%lf\n",pi);
+    printf("%lf",E);
+    return 0;
+}
